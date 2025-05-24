da955db4a5d94edc4b3483c791f0625d319e8181
BibekYogi6462 <bibekyogi6462@gmail.com>
2025-05-24 19:57:09 +0545

    commit

--- Lecture 8 Errors and Debugging/app.js
+++ Lecture 8 Errors and Debugging/app.js
@@ -1,10 +1,12 @@
 const http = require('http');
+const testingSyntax = require('./syntax');
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url, req.method);
+    testingSyntax();
 });
 
 
-const PORT = 3001;
+const PORT = 3000;
 server.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`);
 })
